04aea389e28ec08629db177d06591ca0838840ba
Nolan Peterson <ndpete2007@gmail.com>
2025-02-27 13:08:59 -0600

    generate script

---
+++ generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CMD="npx @openapitools/openapi-generator-cli"
+
+rm -rf api-docs/
+git clone https://github.com/SpaceTradersAPI/api-docs.git
+cd api-docs
+git checkout a2af2dfc59c70b27b81e72c9b4b5576da3ce57b7
+cd ..
+
+npx @openapitools/openapi-generator-cli generate \
+ -i api-docs/reference/SpaceTraders.json \
+ -g rust --skip-validate-spec \
+ --additional-properties=preferUnsignedInt=true \
+ --additional-properties=supportAsync=true \
+ --additional-properties=supportMultipleResponses=true
+
+cargo fmt
